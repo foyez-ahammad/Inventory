92b2c2ba89e32483472baf6da376d9505c291745
foyez-ahammad <manage.foyezahammad@gmail.com>
2022-08-03 11:32:54 +0600

    updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # **Inventory Management Project**
+
+1. First, register your account
+2. second, when you register checkmark is_staff box
+3. third, login and explore.
+
+Live link this projects:
+http://p1-inventory.herokuapp.com/
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('staff', views.staff, name='dashboard-staff'),
-    path('staff/detail/<int:id>/', views.staff_detail, name='dashboard-staff-detail'),
+    path('staff/detail/<int:id>/', views.staff_detail,
+         name='dashboard-staff-detail'),
     path('order', views.order, name='dashboard-order'),
     path('product', views.product, name='dashboard-product'),
     path('product/edit/<int:id>/', views.product_edit,
